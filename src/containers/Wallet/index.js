66f1ab2b7f055728048e494424e2d3bf487e0cb3
salahuddin-itsol-19 <salahuddin@masology.net>
2020-08-31 18:42:02 +0500

    fixes

--- src/containers/Wallet/index.js
+++ src/containers/Wallet/index.js
@@ -1,6 +1,6 @@
 // @flow
-import {connect} from 'react-redux';
-import React, {Component} from 'react';
+import { connect } from 'react-redux';
+import React, { Component } from 'react';
 import {
   Text,
   View,
@@ -17,31 +17,34 @@ import {
 } from 'react-native';
 import styles from './style';
 import Header from '../../components/Header/index';
-import {Images, Metrics, Fonts, Colors} from '../../theme';
-import {Actions} from 'react-native-router-flux';
+import { Images, Metrics, Fonts, Colors } from '../../theme';
+import { Actions } from 'react-native-router-flux';
 import SubmitButton from '../../components/SubmitButton';
-import {Footer} from './../../components';
-import {request as get_wallet} from '../../redux/actions/GetWallet';
+import { Footer } from './../../components';
+import { request as get_wallet } from '../../redux/actions/GetWallet';
 import SpinnerLoader from '../../components/SpinnerLoader';
 import WalletCard from '../../components/WalletCard';
-import {initializeToken} from '../../config/WebServices';
+import { initializeToken } from '../../config/WebServices';
 
 class Wallet extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      headerImage: this.props?.getCompany?.data?.data?.user?.profile_img,
+      headerImage: this.props ?.getCompany ?.data ?.data ?.user ?.profile_img,
       walletInfo: [],
       isloading: true,
     };
   }
 
   UNSAFE_componentWillReceiveProps(nextProps) {
+    console.log("sairirir", nextProps)
+
+
+
     if (nextProps.getWallet) {
       if (
         !nextProps.getWallet.failure &&
         !nextProps.getWallet.isFetching &&
-        nextProps.getWallet.data.data &&
         nextProps.getWallet.data.success
       ) {
         this.setState({
@@ -54,7 +57,7 @@ class Wallet extends Component {
         nextProps.getWallet.data.data &&
         !nextProps.getWallet.data.success
       ) {
-        this.setState({isloading: false});
+        this.setState({ isloading: false });
       }
     }
   }
@@ -64,26 +67,26 @@ class Wallet extends Component {
   };
 
   _renderOverlaySpinner = () => {
-    const {isloading} = this.state; //this.props.getNotAssociateWithCompany;
+    const { isloading } = this.state; //this.props.getNotAssociateWithCompany;
     return <SpinnerLoader isloading={isloading} />;
   };
   renderHeaderHeading = () => {
-    const {walletInfo} = this.state;
+    const { walletInfo } = this.state;
 
     console.log(
       'walletInfowalletInfowalletInfo =>>>>>>><<<<<<',
-      walletInfo?.data?.totalAmount,
+      walletInfo,
     );
 
     return (
-      <View style={{marginVertical: Metrics.ratio(20), alignItems: 'center'}}>
-        <Text numberOfLines={1} style={{fontSize: Metrics.ratio(20)}}>
+      <View style={{ marginVertical: Metrics.ratio(20), alignItems: 'center' }}>
+        <Text numberOfLines={1} style={{ fontSize: Metrics.ratio(20) }}>
           Wallet Points
         </Text>
         <Text
           numberOfLines={1}
-          style={{fontSize: Metrics.ratio(25), color: '#FF3600'}}>
-          {walletInfo?.data?.totalAmount} Pts
+          style={{ fontSize: Metrics.ratio(25), color: '#FF3600' }}>
+          {walletInfo ?.data ?.totalAmount} Pts
         </Text>
         <SubmitButton ButtonText={'1$ = 10 Point'} />
       </View>
@@ -91,7 +94,7 @@ class Wallet extends Component {
   };
 
   render() {
-    const {isloading, walletInfo} = this.state;
+    const { isloading, walletInfo } = this.state;
 
     return (
       <Footer navigation={this.props.navigation.navigate} screen={''}>
@@ -100,14 +103,14 @@ class Wallet extends Component {
             {this.renderHeaderHeading()}
 
             <FlatList
-              data={walletInfo?.data?.transactions}
-              renderItem={({item, index}) => {
+              data={walletInfo ?.data ?.transactions}
+              renderItem={({ item, index }) => {
                 return <WalletCard disable={true} walletInfo={item} />;
               }}
             />
-            {!walletInfo?.data?.transactions.length && !isloading && (
-              <View style={{justifyContent: 'center', alignItems: 'center'}}>
-                <Text style={{fontSize: Metrics.ratio(16)}}>
+            {!walletInfo.length && !isloading && (
+              <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+                <Text style={{ fontSize: Metrics.ratio(16) }}>
                   No Data Found!
                 </Text>
               </View>
@@ -125,6 +128,6 @@ const mapStateToProps = (state) => ({
   getWallet: state.getWallet,
 });
 
-const actions = {get_wallet};
+const actions = { get_wallet };
 
 export default connect(mapStateToProps, actions)(Wallet);
